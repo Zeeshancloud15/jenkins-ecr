2d8a85cb708637f910f97b1ce74727b3feb8ea5c
Md Zeeshan Uddin <zeeshancloud15@gmail.com>
2026-01-12 16:29:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+# Create Flask app
 app = Flask(__name__)
 
+# Define home route
 @app.route("/")
 def hello():
     return "Hello from zeeshan.agency 03!"
 
+# Run the app
 if __name__ == "__main__":
+    # Listen on all IPs and port 80 for Docker
     app.run(host="0.0.0.0", port=80)
-
